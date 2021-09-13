5c3a9baf35402007a88511094776b776fe6ce15c
parkchoongho <pchyo92@gmail.com>
2021-09-13 12:17:45 +0900

    malloc lab

--- mm.c
+++ mm.c
@@ -38,8 +38,6 @@ team_t team = {
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 #define WSIZE 4
-
-
 #define CHUNKSIZE (1<<12)
 
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
@@ -64,6 +62,8 @@ team_t team = {
 
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+static char *heap_listp = NULL;
 
 static void *coalesce(void *bp) {
     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
@@ -106,21 +106,48 @@ static void *extend_heap(size_t words) {
     return coalesce(bp);
 }
 
+static void *find_fit(size_t size) {
+    void *bp;
+
+    for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(HDRP(bp)) && (size <= GET_SIZE(HDRP(bp)))) {
+            return bp;
+        }
+    }
+    return NULL;
+}
+
+static void place(void *bp, size_t asize) {
+    size_t csize = GET_SIZE(HDRP(bp));
+
+    if((csize - asize) >= (2 * ALIGNMENT)) {
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        bp = NEXT_BLKP(bp);
+        PUT(HDRP(bp), PACK(csize - asize, 0));
+        PUT(FTRP(bp), PACK(csize - asize, 0));
+    } else {
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
+}
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
-    char *headp_listp = NULL;
-
-    if((headp_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+    if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
         return -1;
     
-    PUT(headp_listp, 0);
-    PUT(headp_listp + (1*WSIZE), PACK(ALIGNMENT, 1));
-    PUT(headp_listp + (2*WSIZE), PACK(ALIGNMENT, 1));
-    PUT(headp_listp + (3*WSIZE), PACK(0, 1));
-    headp_listp += (2*ALIGNMENT);
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1*WSIZE), PACK(ALIGNMENT, 1));
+    PUT(heap_listp + (2*WSIZE), PACK(ALIGNMENT, 1));
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1));
+    heap_listp += (2*WSIZE);
+
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
+        return -1;
 
     return 0;
 }
@@ -133,22 +160,27 @@ void *mm_malloc(size_t size)
 {
     size_t asize;
     size_t extendsize;
-    
-
-    char *p = mem_sbrk(asize);
+    char *p;
 
     if (size == 0)
         return NULL;
 
-    if (size < ALIGNMENT)
+    if (size <= ALIGNMENT)
+        asize = 2*ALIGNMENT;
+    else
+        asize = ALIGNMENT * ((size + (ALIGNMENT) + (ALIGNMENT - 1)) / ALIGNMENT);
 
-
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+    if ((p = find_fit(asize)) != NULL) {
+        place(p, asize);
+        return p;
     }
+
+    extendsize = MAX(asize, CHUNKSIZE);
+    if((p = extend_heap(extendsize / WSIZE)) == NULL)
+        return NULL;
+    
+    place(p, asize);
+    return p;
 }
 
 /*
@@ -175,7 +207,8 @@ void *mm_realloc(void *ptr, size_t size)
     newptr = mm_malloc(size);
     if (newptr == NULL)
       return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+    // copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+    copySize = GET_SIZE(HDRP(oldptr));
     if (size < copySize)
       copySize = size;
     memcpy(newptr, oldptr, copySize);
